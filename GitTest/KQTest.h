6d7b6b4c560c62c5baebb8d1efb1128a2c8fa859
kingslang001 <kingslang001@gmail.com>
2014-03-18 21:44:28 +0800

    second submit

---
+++ GitTest/KQTest.h
@@ -0,0 +1,13 @@
+//
+//  KQTest.h
+//  GitTest
+//
+//  Created by Qian Wang on 3/18/14.
+//  Copyright (c) 2014 Qian Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KQTest : NSObject
+
+@end
---
+++ GitTest/KQTest.m
@@ -0,0 +1,13 @@
+//
+//  KQTest.m
+//  GitTest
+//
+//  Created by Qian Wang on 3/18/14.
+//  Copyright (c) 2014 Qian Wang. All rights reserved.
+//
+
+#import "KQTest.h"
+
+@implementation KQTest
+
+@end
